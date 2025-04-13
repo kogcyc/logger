98d35326d35522a1a7a0adcb0ac65594d13b02eb
kogcyc <kogcyc@gmail.com>
2025-04-12 17:44:29 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'home page route'
-    
-@app.route('/logger')
+
+@app.route('/logger', methods=['POST'])
 def logger():
-    return 'logger route'
+    data = request.get_json()
+    print("📥 Log received:", data)  # This will show in Vercel's function logs
+
+    return jsonify({
+        "status": "success",
+        "message": "Log received",
+        "received": data
+    }), 200
